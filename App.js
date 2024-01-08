e47b53b241b39e1516f537b37b596b0d2d5c7e99
Jer-Tran <jeremy.tran83@gmail.com>
2024-01-08 19:56:00 +1100

    grid displays as grid properly

--- App.js
+++ App.js
@@ -1,17 +1,34 @@
 import * as LightsOut from './LightsOut.js'
 
-const board = LightsOut.createBoard(5,5,2,0)
+const ROW_LEN = 5
+const COL_LEN = ROW_LEN
+const board = LightsOut.createBoard(ROW_LEN , COL_LEN,2,0)
 // document.getElementById("reset-button").onclick = "createBoard(4, 4, 3, 0)"
 
 console.log(board)
 
 const boardElem = document.querySelector('.board')
-console.log(boardElem)
+const colours = []
+
+boardElem.style.setProperty("--num-cols", COL_LEN)
+boardElem.style.setProperty("--num-rows", ROW_LEN)
 
 board.forEach(row => {
+    const rowElem = document.createElement("div")
+    rowElem.className = "row"
     row.forEach(tile => {
+
         const elem = document.createElement("div")
-        elem.innerHTML = "0"
-        boardElem.append(elem)
+        // elem.innerHTML = tile
+        elem.className = "tile"
+        elem.dataset.status = tile
+
+        // Can't figure out how to make this function work
+        elem.onclick = console.log(5)
+
+        rowElem.append(elem)
     })
+    boardElem.append(rowElem)
 });
+
+console.log(boardElem)
--- LightsOut.js
+++ LightsOut.js
@@ -4,7 +4,14 @@ export function createBoard(rows, cols, states, seed) {
     for (let i = 0; i < rows; i++) {
         const row = []
         for (let j = 0; j < cols; j++) {
-            row.push(0)
+            const tile = {i, j}
+
+            if ((j + i) % 2 == 0) {
+                row.push(1)
+            } else {
+                row.push(0)
+            }
+            
         }
         board.push(row)
     }
