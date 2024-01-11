81df70d3f22026b6345677ff496ea90aaf758c2c
Jer-Tran <jeremy.tran83@gmail.com>
2024-01-11 21:23:23 +1100

    added custom row and column size input

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
 import * as LightsOut from './LightsOut.js'
 
-const ROW_LEN = 5
-const COL_LEN = ROW_LEN
+const DEFAULT_LEN = 5
 let board = null
 
 export function startGame() {
-    board = LightsOut.createBoard(ROW_LEN , COL_LEN,2,0)
+    // Do nothing if input params aren't valid
+    if (document.getElementById("width-in").value == "" ||
+        document.getElementById("height-in").value == "") {
+        console.log("invalid")
+        document.getElementById("input-warning").style.display = "block"
+        return
+    }
+    document.getElementById("input-warning").style.display = "none"
+    
+    let rows = document.getElementById("height-in").valueAsNumber;
+    let cols = document.getElementById("width-in").valueAsNumber;
+    
+    
+    board = LightsOut.createBoard(rows, cols, 2, 0)
 
     const boardElem = document.querySelector('.board')
     const colours = []
 
-    boardElem.style.setProperty("--num-cols", COL_LEN)
-    boardElem.style.setProperty("--num-rows", ROW_LEN)
+    boardElem.style.setProperty("--num-cols", cols)
+    boardElem.style.setProperty("--num-rows", rows)
 
     LightsOut.displayBoard(board, boardElem)
 
-    console.log(board)
 }
 
 startGame()
